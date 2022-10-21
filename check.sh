40d0925053e45209908ea9465ab8899bcbdd59b9
raf181_ <93051606+raf181@users.noreply.github.com>
2022-10-21 11:32:58 +0200

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,38 +1,9 @@
 #!/bin/bash
-
-RED="\e[31m"
-GREEN="\e[32m"
-YELOW="\e[33m"  
-BLUE="\e[34m"
-ENDCOLOR="\e[0m";
-echo 
-echo 
-echo _█████╗_██╗___██╗████████╗_██████╗_███╗___███╗_█████╗_████████╗_██████╗_██████╗ 
-echo ██╔══██╗██║___██║╚══██╔══╝██╔═══██╗████╗_████║██╔══██╗╚══██╔══╝██╔═══██╗██╔══██╗
-echo ███████║██║___██║___██║___██║___██║██╔████╔██║███████║___██║___██║___██║██████╔╝
-echo ██╔══██║██║___██║___██║___██║___██║██║╚██╔╝██║██╔══██║___██║___██║___██║██╔══██╗
-echo ██║__██║╚██████╔╝___██║___╚██████╔╝██║_╚═╝_██║██║__██║___██║___╚██████╔╝██║__██║
-echo ╚═╝__╚═╝ ╚═════╝____╚═╝____╚═════╝ ╚═╝_____╚═╝╚═╝__╚═╝___╚═╝____╚═════╝_╚═╝__╚═╝
-echo 
-uptime
-who
-echo
-if [[ $(/usr/bin/id -u) -ne 0 ]]; then
-    echo -e "${RED}!!! run as root !!!${ENDCOLOR}"
-    echo -e "${RED} or a user with the permissions ${ENDCOLOR}"
-    sleep 5
+cat /etc/issue
+read distro
+if [ $distro -eq Debian ];
+then
+ echo "You are using Debian"
+else
+ echo "you are not using Debian some things may not work"
 fi
-echo 
-
-/etc/os-release
-
-cat
-
-grep
-
-/etc/os-release
-
-source /etc/os-release
-
-cat /etc/issue 
-read distro if [ $distro -eq Debian ] then         echo "You are using Debian" else         echo "you are not using Debian some things may not work" fi
