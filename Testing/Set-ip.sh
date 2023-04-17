8ad0f23b66553ca174d329f48328ab5711599dfe
perla0cd <126961687+perla0cd@users.noreply.github.com>
2023-04-17 19:08:23 +0200

    Update to Set-ip.sh

--- Testing/Set-ip.sh
+++ Testing/Set-ip.sh
@@ -5,17 +5,17 @@ echo "Available network interfaces:"
 echo "$(ip link show | awk -F': ' '{print $2}' | grep -v lo)"
 
 # Prompt the user for the network interface name
-read -p "Enter the network interface name to configure (e.g. eth0): " iface
+read -p -r "Enter the network interface name to configure (e.g. eth0): " iface
 
 # Prompt the user for the network configuration
-read -p "Enter the IP address to set (e.g. 192.168.0.100): " ipaddr
-read -p "Enter the subnet mask (e.g. 255.255.255.0): " subnet
-read -p "Enter the gateway address (e.g. 192.168.0.1): " gateway
-read -p "Enter the primary DNS server (e.g. 8.8.8.8): " dns1
-read -p "Enter the secondary DNS server (e.g. 8.8.4.4): " dns2
+read -p -r "Enter the IP address to set (e.g. 192.168.0.100): " ipaddr
+read -p -r "Enter the subnet mask (e.g. 255.255.255.0): " subnet
+read -p -r "Enter the gateway address (e.g. 192.168.0.1): " gateway
+read -p -r "Enter the primary DNS server (e.g. 8.8.8.8): " dns1
+read -p -r "Enter the secondary DNS server (e.g. 8.8.4.4): " dns2
 
 # Prompt the user for the interface priority
-read -p "Enter the interface priority (optional, default is 0): " priority
+read -p -r "Enter the interface priority (optional, default is 0): " priority
 
 # Set the interface priority to 0 if not provided
 if [[ -z $priority ]]; then
