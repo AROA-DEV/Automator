da11e914af94421f73dd3ae8910cb7ca20c88d44
raf181_ <93051606+raf181@users.noreply.github.com>
2023-04-21 11:56:17 +0200

    V1.5.1
    start of version record and version checker

---
+++ Dedicated/Debian/Update-test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# set the repository URL and current version
+url="https://raw.githubusercontent.com/AROA-DEV/Automator/Beta-testing/Dedicated/Debian/version.txt"
+current_version="1.5.1"
+
+# get the version from the remote file
+remote_version=$(curl -s $url)
+
+# compare the versions
+if [[ "$current_version" != "$remote_version" ]]; then
+    echo "There is a new version available. Please update to version $remote_version."
+else
+    echo "You are using the latest version."
+fi
